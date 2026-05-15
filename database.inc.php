e4556aecf1c2076c5f21112b059294a13b02c976
Captadarsh <adarshjain9god@gmail.com>
2026-05-15 22:27:15 +0530

    Delete database.inc.php

--- database.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con=mysqli_connect('localhost','u158165289_yps','Aayush@9','u158165289_yps');
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
-?>
